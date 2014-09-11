24dd334616d1cb82ce67804fd55d5f8a6cdefbc3
wyrover <wyrover@gmail.com>
2014-09-11 13:53:20 +0800

    * modify README.md

--- README.md
+++ README.md
@@ -3,6 +3,60 @@ ci_guestbook
 
 CodeIgniter guestbook demo
 
+
+## 目录结构
+
+ci_guestbook
+├─application
+│  ├─cache
+│  ├─config
+│  ├─controllers
+│  ├─core
+│  ├─helpers
+│  ├─hooks
+│  ├─language
+│  │  └─english
+│  ├─libraries
+│  ├─logs
+│  ├─models
+│  ├─third_party
+│  └─views
+│      └─errors
+│          ├─cli
+│          └─html
+├─install                                存放安装脚本，包括创建数据库sql文件
+├─system
+│  ├─core
+│  │  └─compat
+│  ├─database
+│  │  └─drivers
+│  │      ├─cubrid
+│  │      ├─ibase
+│  │      ├─mssql
+│  │      ├─mysql
+│  │      ├─mysqli
+│  │      ├─oci8
+│  │      ├─odbc
+│  │      ├─pdo
+│  │      │  └─subdrivers
+│  │      ├─postgre
+│  │      ├─sqlite
+│  │      ├─sqlite3
+│  │      └─sqlsrv
+│  ├─fonts
+│  ├─helpers
+│  ├─language
+│  │  └─english
+│  └─libraries
+│      ├─Cache
+│      │  └─drivers
+│      ├─Javascript
+│      └─Session
+│          └─drivers
+└─test_files
+
+
+
 ## 创建数据库
 
 guestbook
--- application/controllers/guestbook.php
+++ application/controllers/guestbook.php
@@ -3,40 +3,32 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Guestbook extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index() {
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     *      http://example.com/index.php/welcome
+     *  - or -
+     *      http://example.com/index.php/welcome/index
+     *  - or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see http://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index() {
         $this->load->model('Guestbook_model');
-		$data = array();
-		$data["posted"] = false;
-		if( $this->input->post("submit") ) {
-			$data = array(
-				"name" => $this->input->post("name"),
-				"url" => $this->input->post("url"),
-				"comment" => $this->input->post("comment")
-			);
-			if( $this->Guestbook_model->insert( $data ) ) {
-				$data["posted"] = true;
-			}
-		}
-		$data["entries"] = $this->Guestbook_model->view();
-		$this->load->view("guestbook.php", $data);
-	}
-
-    
+        $data = array();
+        $data["posted"] = false;
+        if ($this->input->post("submit")) {
+            $data = array("name" => $this->input->post("name"), "url" => $this->input->post("url"), "comment" => $this->input->post("comment"));
+            if ($this->Guestbook_model->insert($data)) {
+                $data["posted"] = true;
+            }
+        }
+        $data["entries"] = $this->Guestbook_model->view();
+        $this->load->view("guestbook.php", $data);
+    }
 }
-
